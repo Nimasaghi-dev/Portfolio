67edecc81f07700bb27d3e7a6f0fa1e231a02fdf
nima <nimasaghi.dev@gmail.com>
2025-07-31 14:38:47 +0200

    create nested and dynamic route

---
+++ app/about/team/[name]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TeamDetailsPage = () => {
+  return (
+    <div className='text-white text-3xl'>Team member TeamDetailsPage</div>
+  )
+}
+
+export default TeamDetailsPage
