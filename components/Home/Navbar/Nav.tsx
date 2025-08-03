e45ceb61b997e52638f98c9b977b420fc6a6b2a0
nima <nimasaghi.dev@gmail.com>
2025-08-03 15:48:14 +0200

    add outer/logo/logoicon container and put icon inside it with some styles

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,9 +1,19 @@
 // Navbar component - for the desktop navigation bar
 import React from 'react'
+import { FaCode } from 'react-icons/fa';
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div className='transition-all duration-200 h-[12vh] z-[10000] fixed w-full'>
+        <div className='flex justify-between h-full w-[90%] mx-auto items-center'>
+            {/* logo */}
+            <div className='flex items-center space-x-2'>
+                <div className='w-10 h-10 bg-white rounded-full items-center justify-center flex flex-col'>
+                    <FaCode className='w-5 h-5 text-black'/>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
