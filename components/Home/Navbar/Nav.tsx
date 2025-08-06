cfe556c1af8c9244d264cd5e57e6696dcd964eaa
nima <nimasaghi.dev@gmail.com>
2025-08-06 22:56:32 +0200

    implement navlinks and add some style to it

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,4 +1,6 @@
 // Navbar component - for the desktop navigation bar
+import { NavLinks } from '@/constant/constant';
+import Link from 'next/link';
 import React from 'react'
 // import { FaCode } from 'react-icons/fa';
 import { BiAtom } from "react-icons/bi";
@@ -13,6 +15,14 @@ const Nav = () => {
                     <BiAtom className='w-5 h-5 text-black'/>
                 </div>
                 <h1 className='text-white hidden sm:block md:text-2xl text-xl font-bold'>Nima</h1>
+            </div>
+            {/* navLinks */}
+            <div className='hidden lg:flex items-center space-x-10'>
+              {NavLinks.map((link) => {
+                return <Link key={link.id} href={link.url} className='text-base hover:text-cyan-300 text-white font-medium transition-all duration-200'>
+                  <p>{link.label}</p>
+                </Link>
+              })}
             </div>
         </div>
     </div>
