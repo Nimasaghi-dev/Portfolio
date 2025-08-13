5fe59b90d7f44df541c0232129ad807f65bc7e2a
nima <nimasaghi.dev@gmail.com>
2025-08-13 23:44:57 +0200

    add cursor-pointer to the cross icon in hamburger menu

--- components/Home/Navbar/MobileNav.tsx
+++ components/Home/Navbar/MobileNav.tsx
@@ -28,7 +28,7 @@ const MobileNav = ({closeNav, showNav}:Props) => {
         })}
 
       {/* Cross icon */}
-      <CgClose onClick={closeNav} className='absolute top-[0.7rem] right-[1.4rem] sm:w-8 sm:h-8 w-6 h-6 '/>
+      <CgClose onClick={closeNav} className='cursor-pointer absolute top-[0.7rem] right-[1.4rem] sm:w-8 sm:h-8 w-6 h-6 '/>
 
       </div>
     </div>
