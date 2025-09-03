80d4e0b13a0aa4b6fd3578059b649e712c403144
nima <nimasaghi.dev@gmail.com>
2025-09-03 14:44:12 +0200

    feat:add the first card in the experience section and render it

--- components/Home/Resume/Resume.tsx
+++ components/Home/Resume/Resume.tsx
@@ -1,8 +1,23 @@
+import ResumeCard from "./ResumeCard";
+import { FaCodepen } from "react-icons/fa";
 
 const Resume = () => {
   return (
-    <div>Resume</div>
-  )
-}
-
-export default Resume
+    <div className="pt-20 pb-16">
+      <div className="w-[90%] sm:w-[70%] mx-auto grid grid-cols-1 xl:grid-cols-2 gap-10">
+        {/* Work Experience */}
+        <div className="">
+          <h1 className="text-3xl sm:text-4xl font-bold text-white">
+            My Work <span className="text-cyan-200">Experiences</span>
+          </h1>
+          <div className="mt-10">
+            <ResumeCard Icon={FaCodepen} role="Full Stack Developer" />
+          </div>
+        </div>
+        {/* Education */}
+        <div className="">Education</div>
+      </div>
+    </div>
+  );
+};
+export default Resume;
