c3d8cd40cc5896c1a0b659d55443d120689340f1
nima <nimasaghi.dev@gmail.com>
2025-09-04 21:58:56 +0200

    feat:add first project image

--- components/Home/Projects/Projects.tsx
+++ components/Home/Projects/Projects.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Projects = () => {
@@ -6,6 +7,11 @@ const Projects = () => {
         <h1 className='text-center text-2xl md:text-4xl xl:text-5xl font-bold text-white'>
             Recent <span className='text-cyan-300'>Projects</span>
         </h1>
+        <div className='w-[70%] mx-auto grid grid-cols-1 md:grid-cols-2 gap-10 mt-16'>
+            <div>
+                <Image src='/images/NomNom-menu.png' alt='img' width={800} height={650} className='rounded-lg' />
+            </div>
+        </div>
     </div>
   )
 }
