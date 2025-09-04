0222ccb0c0c689fca537688642e52a4e151d1658
nima <nimasaghi.dev@gmail.com>
2025-09-04 17:07:34 +0200

    feat:add role section in ResumeCard

--- components/Home/Resume/ResumeCard.tsx
+++ components/Home/Resume/ResumeCard.tsx
@@ -14,6 +14,13 @@ const ResumeCard = ({Icon, role, date}: Props) => {
             <div className="sm:w-14 sm:h-14 w-10 h-10 bg-blue-950 rounded-full flex items-center justify-center flex-col">
                 <Icon className="sm:w-8 sm:h-8 w-6 h-6 text-white" />
             </div>
+            <div className="flex-1">
+                {date && (
+                    <h1 className="mb-2 sm:px-6 sm:py-1.5 px-4 py-1 rounded-full bg-gray-200 text-gray-600 w-fit sm:text-lg text-sm font-bold">{date}</h1>
+                )}
+                <h1 className="text-gray-200 text-xl sm:text-2xl font-semibold">{role}</h1>
+                <p className="text-gray-200 text-sm sm:text-base pt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, iusto possimus molestias, est alias cupiditate velit libero assumenda ipsa obcaecati praesentium quia ipsam consequuntur ea facilis nobis officiis deleniti. Earum!</p>
+            </div>
         </div>
     </div>
   )
