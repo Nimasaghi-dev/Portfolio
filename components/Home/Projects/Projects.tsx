6478a9638b60c22dcfaebcf08e3f2193b30f63e0
nima <nimasaghi.dev@gmail.com>
2025-09-04 21:38:54 +0200

    feat:create project component-not complete

---
+++ components/Home/Projects/Projects.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div className='pt-16 pb-16'>
+        <h1 className='text-center text-2xl md:text-4xl xl:text-5xl font-bold text-white'>
+            Recent <span className='text-cyan-300'>Projects</span>
+        </h1>
+    </div>
+  )
+}
+
+export default Projects
